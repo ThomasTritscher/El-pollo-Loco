7f9ca485d50b0be2b9f7b3eaa8ab0150da922185
unknown <thomas.tritscher33@googlemail.com>
2021-02-08 11:17:12 +0100

    flickering eliminated

--- game.js
+++ game.js
@@ -14,20 +14,25 @@ function init() {
   canvas = document.getElementById('canvas'); // Html Element
   ctx = canvas.getContext("2d"); // der Bereich wo gemalt wird
 
-  setInterval(function () {
-    
-    drawBackground();
-    updateCharacter();
-  }, 50);// Hintergund + Charakter werden alle 50ms gemalt
-
+    draw();
+  
   listenForKeys();
 
 
 }
+function draw() {
+
+  drawBackground();
+  updateCharacter();
+  requestAnimationFrame(draw);// zeichent nach Möglichkeit
+
+}
+
+
 function updateCharacter() {
   let base_image = new Image();// Variable
   base_image.src = 'img/charakter_1.png';// Quelle
-  base_image.onload = function () { //Image wird gezeichnet
+  if(base_image.complete) { //Image wird geladen
     ctx.drawImage(base_image, character_x, 250, base_image.width * 0.35, base_image.height * 0.35); // Parameter der Zeichnung
   };
 
@@ -47,24 +52,24 @@ function drawGround() {
   ctx.fillStyle = "#FFE699";
   ctx.fillRect(0, 375, canvas.width, canvas.height - 375);// Boden wird gezeichnet
 
-  if(isMovingRight) {
+  if (isMovingRight) {
     bg_elements = bg_elements - 2;
   }
 
-  if(isMovingLeft) {
+  if (isMovingLeft) {
     bg_elements = bg_elements + 2;
   }
   //Kaktus1
   let base_image = new Image();// Variable
   base_image.src = 'img/bg_elem_1.png';// Quelle
-  base_image.onload = function () { //Image wird gezeichnet
+  if(base_image.complete) { //Image wird gezeichnet
     ctx.drawImage(base_image, bg_elements, 195, base_image.width * 0.6, base_image.height * 0.6); // Parameter der Zeichnung
   };
 
   //Kaktus2
   let base_image2 = new Image();// Variable
   base_image2.src = 'img/bg_elem_2.png';// Quelle
-  base_image2.onload = function () { //Image wird gezeichnet
+  if(base_image2.complete) { //Image wird gezeichnet
     ctx.drawImage(base_image2, 200 + bg_elements, 195, base_image.width * 0.6, base_image.height * 0.6); // Parameter der Zeichnung
   };
 }
@@ -73,7 +78,7 @@ function listenForKeys() { //bei Tastendruck Pfeil rechts
   document.addEventListener("keydown", e => {
     const k = e.key;
     console.log(k == 'ArrowRight')
-    
+
     if (k == 'ArrowRight') {
       isMovingRight = true;
       character_x = character_x + 5; //um 10px auf X-Achse weiter
@@ -89,15 +94,15 @@ function listenForKeys() { //bei Tastendruck Pfeil rechts
   document.addEventListener("keyup", e => { //loslassen der Taste
     const k = e.key;
     console.log(k == 'ArrowRight')
-    
+
     if (k == 'ArrowRight') {
       isMovingRight = false;
-      character_x = character_x + 5; 
+      character_x = character_x + 5;
     }
 
     if (k == 'ArrowLeft') {
       isMovingLeft = false;
-      character_x = character_x - 5; 
+      character_x = character_x - 5;
     }
 
   });
